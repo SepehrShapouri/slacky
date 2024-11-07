1fd804e30c6e70e67293ce3fcf30bd56f45761b4
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-11-07 12:52:07 +0330

    fix:fix prisma build

--- src/hooks/use-socket.ts
+++ src/hooks/use-socket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || 'http://localhost:3000';
+const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL
 
 export const useSocket = (namespace: 'channels' | 'workspaces') => {
   const [socket, setSocket] = useState<Socket | null>(null);
